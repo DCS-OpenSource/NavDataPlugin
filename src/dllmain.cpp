0aa64bae8ce900ebafb501260443c2c4ead23ac4
BrianTheBrain11 <68790720+BrianTheBrain11@users.noreply.github.com>
2025-06-05 11:32:12 -0700

    cmake dll setup

---
+++ src/dllmain.cpp
@@ -0,0 +1,10 @@
+#include <Windows.h>
+
+BOOL APIENTRY DLLMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
+{
+    switch (ul_reason_for_call)
+    {
+        
+    }
+    return TRUE;
+}
