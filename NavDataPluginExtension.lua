ff4dffc1fb60b429ae6b68a47992aa9d2a3dbc79
blueberrypi-studio <handyhaydn22@gmail.com>
2025-06-06 12:53:06 +1200

    fixed the dll loader, dlls now run

--- NavDataPluginExtension.lua
+++
@@ -1,7 +0,0 @@
-local NavData_dll_path = LockOn_Options.script_path.."NavDataPlugin/"
-
-package.cpath = package.cpath .. ";" .. NavData_dll_path .. "/?.dll"
-success, result = pcall(require, 'NavDataPluginExtension')
-if not success then
-    print_message_to_user("NavDataPluginExtension: Failed to load NavDataPluginExtension. Please check the path: " .. NavData_dll_path)
-end
