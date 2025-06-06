6102f879090e91828054a19c044541d07d2cabdb
blueberrypi-studio <handyhaydn22@gmail.com>
2025-06-06 12:36:04 +1200

    non working

---
+++ src/NavDataPluginExtension.h
@@ -0,0 +1,9 @@
+#pragma once
+
+struct lua_State;
+
+namespace NavDataPluginExtension
+{
+    extern int l_CreateNavDataPluginExtension( lua_State* L );
+}
+
--- src/dllmain.cpp
+++ src/dllmain.cpp
@@ -1,10 +1,33 @@
 #include <Windows.h>
 
+extern "C"
+{
+#include "lua.h"
+}
+
+
+#include "NavDataPluginExtension.h"
+#include <optional>
+
 BOOL APIENTRY DLLMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
 {
     switch (ul_reason_for_call)
     {
-        
+        case DLL_PROCESS_ATTACH:
+            break;
+        case DLL_THREAD_ATTACH:
+            break;
+        case DLL_THREAD_DETACH:
+            break;
+        case DLL_PROCESS_DETACH:
+            break;
     }
     return TRUE;
+}
+
+
+
+extern "C" int __declspec( dllexport ) luaopen_NavDataPluginExtension( lua_State* L )
+{
+    return NavDataPluginExtension::l_CreateNavDataPluginExtension(L);
 }
