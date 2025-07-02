8942d3b10105ac9b602248091e662f53a7adf7fc
blueberrypi-studio <handyhaydn22@gmail.com>
2025-07-03 11:17:29 +1200

    removed Navigraph stuff from main for now

--- src/NavDataPluginNaviGraph.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-struct lua_State;
-
-namespace NavDataPluginNaviGraph
-{
-    extern int l_CreateNavDataPluginNaviGraph( lua_State* L );
-}
-
--- src/NavDataPluginSimbrief.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-struct lua_State;
-
-namespace NavDataPluginNaviGraph
-{
-    int l_getLatestSimbriefOFP(lua_State* L);
-    int l_setSimbriefUserName(lua_State* L);
-}
